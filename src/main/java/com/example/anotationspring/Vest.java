c4eba05ce2b9a1c52d7b2b78705e13f1763f96f0
nguyennamcuong <cuong@gmail.com>
2022-12-09 13:58:33 +0700

    @Scope

--- src/main/java/com/example/anotationspring/Vest.java
+++ src/main/java/com/example/anotationspring/Vest.java
@@ -1,8 +1,10 @@
 package com.example.anotationspring;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class Vest implements Outfit{
     @Override
     public void wear() {
